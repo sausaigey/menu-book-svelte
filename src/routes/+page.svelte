cb1a954dae67b6c6cd2ec00e7177d49d81cf0ab9
shannon <143071081+sausaigey@users.noreply.github.com>
2024-02-29 07:23:50 +0000

    copy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,91 +1,73 @@
 <script lang="ts">
 	import MenuTile from '../lib/component/MenuTile.svelte';
 
-  const menu = [
-    {
-      photoUrl: 
-	  	'https://images.immediate.co.uk/production/volatile/sites/30/2014/05/Epic-summer-salad-hub-2646e6e.jpg?resize=960%2C503',
-      name: 
-	  	'Avocado Pesto Salad',
-      description: 
-	  	'Home-grown vegetables with loads of avocado, topped with mouthwatering pesto salad',
-	  prices: [
+	const menus = [
 		{
-			label: '',
-			price: 35
-
+			photoUrl:
+				'https://images.immediate.co.uk/production/volatile/sites/30/2014/05/Epic-summer-salad-hub-2646e6e.jpg?resize=960%2C503',
+			name: 'Avocado Pesto Salad',
+			description:
+				'Home-grown vegetables with loads of avocado, topped with mouthwatering pesto salad',
+			prices: [
+				{
+					label: '',
+					price: 35
+				},
+				{
+					label: '+ Grilled Salmon',
+					price: 55
+				}
+			]
 		},
 		{
-			label: '+ Grilled Salmon',
-			price: 55
-
-		}
-	]
-  
-    },
-    {
-      photoUrl: 
-	 	 'https://www.recipetineats.com/wp-content/uploads/2023/05/Garlic-cheese-pizza_9.jpg', 
-      name: 
-		  'Triple Cheese Pizza',
-      description: 
-	  	'Indulge yourself in this simple yet delicious delicacy',
-	  prices: [
-		{
-			label: '1 slice',
-			price: 15
-
+			photoUrl:
+				'https://www.recipetineats.com/wp-content/uploads/2023/05/Garlic-cheese-pizza_9.jpg',
+			name: 'Triple Cheese Pizza',
+			description: 'Indulge yourself in this simple yet delicious delicacy',
+			prices: [
+				{
+					label: '1 slide',
+					price: 15
+				},
+				{
+					label: 'Pan (6 slices)',
+					price: 60
+				}
+			]
 		},
 		{
-			label: 'Pan (6 slices)',
-			price: 60
-
-		}
-	]
-  
-    },
-    {
-      photoUrl: 
-	  	'https://images.theconversation.com/files/525691/original/file-20230511-19-w9pz4k.jpg?ixlib=rb-1.1.0&rect=5%2C2%2C1905%2C1276&q=20&auto=format&w=320&fit=clip&dpr=2&usm=12&cs=strip',
-      name: 
-	  	'Seasonal Wine',
-      description: 
-	  	'A great meal becomes perfect when combined with a good wine',
-	  prices: [
-		{
-			label: 'Glass',
-			price: 100
-
+			photoUrl:
+				'https://images.theconversation.com/files/525691/original/file-20230511-19-w9pz4k.jpg?ixlib=rb-1.1.0&rect=5%2C2%2C1905%2C1276&q=20&auto=format&w=320&fit=clip&dpr=2&usm=12&cs=strip',
+			name: 'Seasonal Wine',
+			description: 'A great meal becomes perfect when combined with a good wine',
+			prices: [
+				{
+					label: 'Glass',
+					price: 100
+				},
+				{
+					label: 'Bottle',
+					price: 1500
+				}
+			]
 		},
 		{
-			label: 'Bottle',
-			price: 1500
-
+			photoUrl:
+				'https://sp-ao.shortpixel.ai/client/to_webp,q_glossy,ret_img,w_512,h_512/https://www.adityabirlacapital.com/healthinsurance/active-together/wp-content/uploads/2019/12/How-Are-Espressos-Good-For-Health_blog-1.gif',
+			name: 'Coffee',
+			description: 'The best coffee you can have from the land of Indonesia',
+			prices: [
+				{
+					label: 'Americano',
+					price: 25
+				},
+				{
+					label: 'Latte',
+					price: 30
+				}
+			]
 		}
-	]
-
-    },
-    {
-      photoUrl: 
-	  	'https://sp-ao.shortpixel.ai/client/to_webp,q_glossy,ret_img,w_512,h_512/https://www.adityabirlacapital.com/healthinsurance/active-together/wp-content/uploads/2019/12/How-Are-Espressos-Good-For-Health_blog-1.gif',
-      name:
-	  	'Coffee',
-      description: 
-	  	'The best coffee you can have from the land of Indonesia',
-	  prices: [
-		{
-			label: 'Americano',
-			price: 25
-
-		},
-		{
-			label: 'Latte',
-			price: 30
-
-		}
-	]
-    }
-  ]; 
+	];
 </script>
 
 <head>
@@ -113,15 +95,16 @@
 		<div class="price-note">All prices are in thousand rupiah</div>
 	</div>
 	<div class="menu-area">
-		{#each menu as menu}
-			<MenuTile photoUrl={menu.photoUrl} name={menu.name} description={menu.description} /> 
-		{/each} 
+		{#each menus as menu}
+			<MenuTile photoUrl={menu.photoUrl} name={menu.name} description={menu.description} />
+		{/each}
 	</div>
 </body>
 
 <style>
 	/** CSS Reset */
 	*,
+	html,
 	body {
 		font-family: 'Barlow';
 		margin: 0;
@@ -173,7 +156,7 @@
 
 	div.menu-area {
 		display: grid;
-		grid-template-columns:repeat(2, minmax(0, 1fr));
+		grid-template-columns: repeat(2, minmax(0, 1fr));
 		gap: 16px;
 	}
 
@@ -182,7 +165,7 @@
 			grid-template-columns: repeat(3, minmax(0, 1fr));
 		}
 	}
-	
+
 	@media (min-width: 800px) {
 		div.menu-area {
 			grid-template-columns: repeat(4, minmax(0, 1fr));
